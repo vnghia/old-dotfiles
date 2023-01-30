3ef432cf31359680cdacdb20c66619e29d034421
Vo Van Nghia <vanvnghia@gmail.com>
2023-01-30 19:11:04 +0100

    python(setup): add download git

--- python/setup/__init__.py
+++ python/setup/__init__.py
@@ -1,7 +1,7 @@
 import shutil
 import subprocess
 from pathlib import Path
-from typing import Optional
+from typing import Literal, Optional
 
 from python.utils.download import download_to_file
 from python.utils.input import read_binary
@@ -28,11 +28,22 @@ class SetupBase:
         self.bin_path = Path(bin_path_str) if bin_path_str else None
 
         self.suffix = suffix
+        self.installed = False
 
+    def download(
+        self,
+        download_type: Literal["file", "git"] = "file",
+        dest: Optional[Path] = None,
+        *args: str,
+    ):
         if self.should_install:
-            self.dest_file = download_to_file(
-                self.url, prompt=self.prompt, suffix=self.suffix
-            )
+            if download_type == "file":
+                self.dest_file = download_to_file(
+                    self.url, dest, self.prompt, suffix=self.suffix
+                )
+            else:
+                dest.parent.mkdir(parents=True, exist_ok=True)
+                subprocess.check_call(["git", "clone", self.url, dest] + list(args))
 
 
 class SetupScript(SetupBase):
@@ -42,8 +53,11 @@ class SetupScript(SetupBase):
         super().__init__(url, command, prompt, suffix)
 
     def execute(self, prefix_cmd: str = "", suffix_cmd: str = "", **kwargs):
-        if self.should_install:
-            return subprocess.run(
-                prefix_cmd.split() + [self.dest_file.name] + suffix_cmd.split(),
-                **kwargs,
-            )
+        if not self.installed:
+            self.download()
+            if self.should_install:
+                return subprocess.run(
+                    prefix_cmd.split() + [self.dest_file.name] + suffix_cmd.split(),
+                    **kwargs,
+                )
+            self.installed = True
