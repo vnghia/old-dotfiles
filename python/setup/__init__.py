e33f8480985c541a01ccdc8f6113bf17a02a3d53
Vo Van Nghia <vanvnghia@gmail.com>
2023-01-30 22:14:19 +0100

    python(setup): add install archive

--- python/setup/__init__.py
+++ python/setup/__init__.py
@@ -1,6 +1,8 @@
 import os
 import shutil
+import stat
 import subprocess
+import tempfile
 from pathlib import Path
 from typing import Literal, Optional
 
@@ -61,4 +63,60 @@ class SetupScript(SetupBase):
                     prefix_cmd.split() + [self.dest_file.name] + suffix_cmd.split(),
                     **kwargs,
                 )
+            self.installed = True
+
+
+class SetupPackage(SetupBase):
+    def __init__(
+        self,
+        url: str,
+        command: str,
+        prompt: Optional[str] = None,
+        suffix: str = ".sh",
+        download_type: Literal["file", "git"] = "file",
+    ):
+        super().__init__(url, command, prompt, suffix)
+        self.download_type = download_type
+
+    @staticmethod
+    def link_binsrc_bin(bin_dir: Path, binsrc_dir: Path, pattern: str):
+        for bin in binsrc_dir.glob(pattern):
+            linked_bin = bin_dir / bin.name
+            linked_bin.unlink(missing_ok=True)
+            print(f"Linking {bin} to {linked_bin}")
+            (bin_dir / bin.name).symlink_to(bin)
+
+    def install(
+        self,
+        bin_dir: Optional[Path] = None,
+        dest_dir: Optional[Path] = None,
+        pattern: str = "bin/*",
+        skip_first_level=True,
+    ):
+        dest_dir = dest_dir or Path(os.environ["CODE_BINSRC"])
+        bin_dir = bin_dir or Path(os.environ["CODE_BIN"])
+        dest_dir.mkdir(parents=True, exist_ok=True)
+        bin_dir.mkdir(parents=True, exist_ok=True)
+        if not self.installed:
+            if self.download_type == "file":
+                self.download(self.download_type)
+                if not self.suffix:
+                    bin_path = bin_dir / self.command
+                    print(f"Saving to {bin_path}")
+                    shutil.copy(self.dest_file.name, bin_path)
+                    bin_path.chmod(bin_path.stat().st_mode | stat.S_IEXEC)
+                else:
+                    binsrc_dir = dest_dir / self.command
+                    if skip_first_level:
+                        with tempfile.TemporaryDirectory() as tempdir_str:
+                            tempdir = Path(tempdir_str)
+                            shutil.unpack_archive(self.dest_file.name, tempdir)
+                            subdir = [dir for dir in tempdir.glob("*") if dir.is_dir()][
+                                0
+                            ]
+                            shutil.copytree(subdir, binsrc_dir, dirs_exist_ok=True)
+                    else:
+                        shutil.unpack_archive(self.dest_file.name, binsrc_dir)
+                    self.link_binsrc_bin(bin_dir, binsrc_dir, pattern)
+            print()
             self.installed = True
